a674bd70bcbecee9885ccb399f5762572b2d00d7
Kevin Chang <kevinjchang98@gmail.com>
2022-08-21 04:50:48 -0700

    Time: 6 ms (39.20%), Space: 6.7 MB (23.46%) - LeetHub

---
+++ 1265-print-immutable-linked-list-in-reverse/1265-print-immutable-linked-list-in-reverse.cpp
@@ -0,0 +1,27 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * class ImmutableListNode {
+ * public:
+ *    void printValue(); // print the value of the node.
+ *    ImmutableListNode* getNext(); // return the next node.
+ * };
+ */
+
+class Solution {
+public:
+    void printLinkedListInReverse(ImmutableListNode* head) {
+        stack<ImmutableListNode*> s;
+        
+        ImmutableListNode* curr = head;
+        while (curr != nullptr) {
+            s.push(curr);
+            curr = curr->getNext();
+        }
+        
+        while (!s.empty()) {
+            s.top()->printValue();
+            s.pop();
+        }
+    }
+};
