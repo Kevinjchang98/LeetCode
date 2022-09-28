cb36edd162b32626cd459f4dddbbcd243f8497b8
Kevin Chang <kevinjchang98@gmail.com>
2022-09-28 03:10:18 -0700

    Time: 0 ms (100.00%), Space: 5.9 MB (30.10%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -6,10 +6,7 @@ public:
         while (x) {
             int curr = x % 10;
             
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && curr > 7)) 
-                return 0;
-            
-            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && curr < -8))
+            if (ans > INT_MAX / 10 || ans < INT_MIN / 10) 
                 return 0;
             
             ans = ans * 10 + curr;
