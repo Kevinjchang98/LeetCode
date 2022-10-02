1d97e9a82d24d32daaa7bf35f54e518b93321428
Kevin Chang <kevinjchang98@gmail.com>
2022-10-01 23:08:56 -0700

    Time: 78 ms (16.67%), Space: 13.2 MB (100.00%) - LeetHub

--- 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
+++ 2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.cpp
@@ -5,28 +5,14 @@ public:
         
         for (int i = 0; i <= grid.size() - 3; i++) {
             for (int j = 0; j <= grid[0].size() - 3; j++) {
-                vector<vector<int>> newV = {{grid[i][j], grid[i][j + 1], grid[i][j + 2]},
-                                            {grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2]},
-                                            {grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]}};
+                int sum = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
+                    + grid[i + 1][j + 1]
+                    + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
                 
-                ans = max(ans, returnSum(newV));
-                cout << ans << endl;
+                ans = max(ans, sum);
             }
         }
             
-        return ans;
-    }
-    
-    int returnSum(vector<vector<int>>& grid) {
-        int ans = 0;
-
-        for (int i = 0; i < 3; i++) {
-            ans += grid[0][i];
-            ans += grid[2][i];
-        }
-        
-        ans += grid[1][1];
-        
         return ans;
     }
 };
