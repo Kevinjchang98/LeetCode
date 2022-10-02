58c895674687d98676352155c04418010e56bb63
Kevin Chang <kevinjchang98@gmail.com>
2022-10-01 22:28:56 -0700

    Time: 1359 ms (12.50%), Space: 6.9 MB (87.50%) - LeetHub

---
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int dp[40000] = {};
+
+    int helper(int i, const string& s) {
+        if (dp[i] == 0) {
+            dp[i] = 1;
+            
+            for (int j = 1; i + 2 * j <= s.size(); j++) {
+                // if j letters from index i of s == j letters from idx i + j of s
+                if (s.compare(i, j, s, i + j, j) == 0)
+                    dp[i] = max(dp[i], 1 + helper(i + j, s));
+            }
+        }
+        
+        return dp[i];
+    }
+    
+    int deleteString(string s) {
+        return helper(0, s);
+    }
+};
