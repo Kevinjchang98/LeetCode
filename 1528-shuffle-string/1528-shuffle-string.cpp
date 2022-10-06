bb461e30b5730eaece110b291c915c331d5d1178
Kevin Chang <kevinjchang98@gmail.com>
2022-10-06 01:53:26 -0700

    Time: 24 ms (18.98%), Space: 15.8 MB (16.62%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        string ans = s;
+        vector<pair<char, int>> c;
         
         for (int i = 0; i < s.size(); i++) {
-            ans[indices[i]] = s[i];
+            c.push_back({s[i], indices[i]});
+        }
+        
+        sort(c.begin(), c.end(),
+             [](pair<char, int> a, pair<char, int> b) {
+                 return a.second < b.second;
+             });
+        
+        string ans;
+        
+        for (auto e : c) {
+            ans += e.first;
         }
         
         return ans;
