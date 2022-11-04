348bdea91724281b1f158997fc91188a40e62244
Kevin Chang <kevinjchang98@gmail.com>
2022-11-04 01:07:59 -0700

    Time: 14 ms (92.68%), Space: 7 MB (76.72%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = "";
+        pair<int, int> ans = {0, 0};
         
-        auto find = [&s](int l, int r) {
-            while (l >= 0 && r < s.size() && s[l] == s[r]) {
-                l--;
-                r++;
-            }
+        for (int i = 0; i < s.size(); i++) {
+            auto a = find(i, i, s);
+            auto b = find(i, i + 1, s);
             
-            return s.substr(++l, r - l);
-        };
-        
-        for (int i = 0; i < s.size(); i++) {            
-            vector<string> curr = {find(i, i), find(i, i+1), ans};
-            
-            ans = *max_element(curr.begin(), curr.end(),
-                               [](string a, string b) {
-                                  return a.size() < b.size();
-                               });
+            if (a.second - a.first > b.second - b.first
+               && a.second - a.first > ans.second - ans.first)
+                ans = a;
+            else if (b.second - b.first > ans.second - ans.first)
+                ans = b;
         }
         
-        return ans.size() == 0 ? s.substr(0, 1) : ans;
+        return ans.second - ans.first == 0 
+            ? s.substr(0, 1) 
+            : s.substr(ans.first + 1, ans.second - ans.first - 1);
+    }
+    
+    pair<int, int> find(int l, int r, const string& s) {
+        while (l >= 0 && r < s.size() && s[l] == s[r]) {
+            l--;
+            r++;
+        }
+        
+        return {l, r};
     }
 };
